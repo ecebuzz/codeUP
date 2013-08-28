bb21a699ea9ed7cae13c4a7c2dff36cfc9a8097e
kelvinih <kelvinwsch@gmail.com>
2013-08-28 15:54:23 -0600

    AlienLanguage tested

--- AlienLanguage/AlienLanguage.java
+++ AlienLanguage/AlienLanguage.java
@@ -7,11 +7,15 @@ import java.util.ArrayList;
 
 public class AlienLanguage {
 	public static void main( String args[] ) throws Exception {
-		String fileNamePart = "test";
+//		String fileNamePart = "test";
+//		String fileNamePart = "A-small-practice";
+		String fileNamePart = "A-large-practice";
+
 		String inputFileName = fileNamePart + ".in";
 		String outputFileName = fileNamePart + ".out";
 		File inputFile = new File( inputFileName );
 		Scanner in = new Scanner( inputFile );
+		PrintWriter out = new PrintWriter( outputFileName );
 		int L = 0, D = 0, N = 0;	
 		if( in.hasNextLine() ) {
 			String[] line = in.nextLine().split( " " );
@@ -23,11 +27,18 @@ public class AlienLanguage {
 			N = Integer.parseInt( line[2] );
 		}
 		HashSet<String> dict = new HashSet<String>();
+		HashSet[] posDict = new HashSet[L];
+		for( int i = 0; i < L; i++ ) {
+			posDict[i] = new HashSet<String>();
+		}
 		for( int i = 0; i < D; i++ ) {
 			if( in.hasNextLine() ) {
 				String line = in.nextLine();
 				if( line.length() == L ) {
 					dict.add( line );
+					for( int k = 0; k < L; k++ ) {
+						posDict[k].add( Character.toString( line.charAt(k) ) );
+					}
 				}
 				else {
 					throw new Exception( "Input File Error: The word length is not L!" );
@@ -52,13 +63,16 @@ public class AlienLanguage {
 		
 
 		for( int i = 0; i < N; i++ ) {
+			testcase[i].buildPosHash( posDict );
 			testcase[i].decipher( dict );
 		}
 
 		for( int i = 0; i < N; i++ ) {
 			ArrayList<String> result = testcase[i].getValidWords();
-			System.out.printf( "Case #%d: %d\n", i, result.size() );
+			System.out.printf( "Case #%d: %d\n", i + 1, result.size() );
+			out.printf( "Case #%d: %d\n", i + 1, result.size() );
 		}
+		out.close();
 
 	}
 }
--- AlienLanguage/AlienMessage.java
+++ AlienLanguage/AlienMessage.java
@@ -4,29 +4,78 @@ import java.util.HashSet;
 public class AlienMessage {
 	private String msg;
 	public ArrayList<String> validWords;
+	public HashSet[] hashWord;
 	private int length;
 	
 	public AlienMessage( String msg, int length ) {
 		this.msg = msg;
 		this.length = length;
 		validWords = new ArrayList<String>();
+		this.hashWord = new HashSet[length];
+		for( int i = 0; i < length; i++ ) {
+			hashWord[i] = new HashSet<String>();
+		}
 	}
 
 	public ArrayList<String> getValidWords() {
 		return validWords;
 	}
 
+	public void buildPosHash( HashSet[] posDict ) {
+		if( msg.isEmpty() ) { return; }
+		String str = msg;
+		
+		for( int i = 0; i < length; i++ ) {
+			int subStrIdx = 0;
+			if( str.charAt(0) == '(' ) {
+				for( int k = 1; k < str.length(); k++ ) {
+					if( str.charAt(k) == ')' ) { 
+						subStrIdx = k;
+						break;						
+					}
+					String charStr = Character.toString( str.charAt(k) );
+					if( posDict[i].contains( charStr ) ) {
+						hashWord[i].add( charStr );
+					}
+				}
+			}
+			else {
+				String charStr = Character.toString( str.charAt(0) );
+				if( posDict[i].contains( charStr ) ) {
+					hashWord[i].add( charStr );
+				}
+				subStrIdx = 0;
+			}
+			str = str.substring( subStrIdx + 1 );
+		}
+	}
 	public void decipher( HashSet<String> dict ) {
 		if( msg == null ) { return; }
-		ArrayList<String> list = decode( msg );
-		for( String element : list ) {
-			if( dict.contains( element ) ) {
-				validWords.add( element );
+		
+//		ArrayList<String> list = decode( msg, 0, posDict );
+//		for( String element : list ) {
+//			if( dict.contains( element ) ) {
+//				validWords.add( element );
+//			}
+//		}
+		
+		for( String validWord : dict ) {
+			boolean valid = true;
+			for( int i = 0; i < length; i ++ ) {
+				if( !hashWord[i].contains( Character.toString( validWord.charAt(i) ) ) ) {
+					valid = false;
+					break;
+				}
+			}
+			if( valid ) {
+				validWords.add( validWord );
 			}
 		}
 	}
-
-	public ArrayList<String> decode( String str ) {
+	
+	
+	// Not efficient!
+	public ArrayList<String> decode( String str, int pos, HashSet[] posDict ) {
 		if( str.isEmpty() ) { return new ArrayList<String>(); }
 		ArrayList<String> curList = new ArrayList<String>();
 		int subStrIdx = 0;
@@ -37,22 +86,24 @@ public class AlienMessage {
 					subStrIdx = i;
 					break;
 				}
-				curList.add( Character.toString( str.charAt(i) ) );
+				String charStr = Character.toString( str.charAt(i) );
+				if( posDict[pos].contains( charStr ) ) {
+					curList.add( charStr );
+				}
 			}
 		}
 		else {
-			for( int i = 0; i < str.length(); i++ ) {
-				if( str.charAt(i) == '(' ) {
-					subStrIdx = i;
-					break;
-				}
-				curList.add( Character.toString( str.charAt(i) ) );
+			String charStr = Character.toString( str.charAt(0) );
+			if( posDict[pos].contains( charStr ) ) {
+				curList.add( charStr );
 			}
+			subStrIdx = 0;
 		}
-		if( curList.size() == str.length() ) {
-			subStrIdx = str.length();
+
+		ArrayList<String> subList = null;
+		if( !curList.isEmpty() ) {
+			subList = decode( str.substring( subStrIdx + 1 ), pos + 1, posDict );
 		}
-		ArrayList<String> subList = decode( str.substring( subStrIdx + 1 ) );
 		ArrayList<String> result = new ArrayList<String>();
 
 		for( String element : curList ) {
