8b2ea234392fbd89b187e215013fd2ea6bc179e1
kelvinih <kelvinwsch@gmail.com>
2013-09-30 15:15:15 -0600

    Quicksums test

--- QuickSums/QuickSums.java
+++ QuickSums/QuickSums.java
@@ -1,9 +1,26 @@
+import java.util.HashMap;
 
 public class QuickSums {
 	public static void main(String[] args) {
-		String numbers = "99999";
-		int sum = 45;
-		system.out.println(minSums(numbers, sum));
+//		//Testcase 0
+//		String numbers = "99999";
+//		int sum = 45;
+//		//Testcase 1
+//		String numbers = "1110";
+//		int sum = 3;
+//		//Testcase 2
+//		String numbers = "0123456789";
+//		int sum = 45;
+//		//Testcase 3
+//		String numbers = "99999";
+//		int sum = 100;
+//		//Testcase 4
+//		String numbers = "382834";
+//		int sum = 100;
+		//Testcase 5
+		String numbers = "9230560001";
+		int sum = 71;
+		System.out.println(minSums(numbers, sum));
 	}
 
 	public static int minSums(String numbers, int sum) {
@@ -13,40 +30,70 @@ public class QuickSums {
 		if(numbers == null) { return -1; }
 		if(numbers.length() == 0) { return -1; }
 
-		HashMap<String, Integer> partialRes = new HashMap<String, Integer>();
-		return computeSums(numbers, sum, partialRes);
+		HashMap<String, HashMap<Integer, Integer>> partialRes = new HashMap<String, HashMap<Integer, Integer>>();
+		computeSums(numbers, sum, partialRes);
+		if(partialRes.containsKey(numbers)) {
+			HashMap<Integer, Integer> map = partialRes.get(numbers);
+			if(map.containsKey(sum)) {
+				return map.get(sum);
+			}
+		}
+		return -1;
 	}
 
-	public static int computeSums(String numbers, int sum, HashMap<String, Integer> partialRes ) {
+	public static void computeSums(String numbers, int sum, HashMap<String, HashMap<Integer, Integer>> partialRes) {
 
+//		if(partialRes.containsKey(numbers)) {
+//			HashMap<Integer, Integer> map = partialRes.get(numbers);
+//			if(map.containsKey(sum)) {
+//				return ;
+//			}
+//		}
+		// sum to depth map
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for(int i = 0; i < numbers.length(); i++) {
-			int num = Integer.parseInt(numbers.substring(0, i));
+			int num = Integer.parseInt(numbers.substring(0, i + 1));
 			// Trailing zeors
 			if(num == 0 && i != numbers.length() - 1) {
 				continue;
 			}
-			if(i == number.length() - 1) {
-				if(num == sum) {
-					return 1;
+			if(i == numbers.length() - 1) {
+				if(num == sum && !map.containsKey(sum)) {
+					map.put(sum, 0);
 				}
 				else {
-					return -1;
+					map.put(num, 0);
 				}
+				break;
 			}
 			if(num <= sum) {
 				String left = numbers.substring(i + 1);
-				if(partialRes.contains(left)) {
-					return partialRes.get(left);
+				if(!partialRes.containsKey(left)) {
+					computeSums(left, sum - num, partialRes); 
+					
+				}
+				int curDepth = -1;
+				
+				HashMap<Integer, Integer> sumToDepth =	partialRes.get(left);
+				if(sumToDepth.containsKey(sum - num)) {
+					map.put(sum, sumToDepth.get(sum - num) + 1);
 				}
 				else {
-					
-					int depth = computeSums(left, sum - num, partialRes) 
-					partialRes.put(left, depth);
+					for(int element : sumToDepth.keySet()) {
+						if(!map.containsKey(element+num)) {
+							map.put(element + num, sumToDepth.get(element) + 1);
+						}
+					}
 				}
+				
 
-					
-				}
+			}
+			else {
+				break;
 			}
 		}
+		partialRes.put(numbers, map);
+
+		
 	}
 }
